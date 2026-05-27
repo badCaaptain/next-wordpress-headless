e72af3587ba7a84720d0861f6916cbe7f1ae7f28
Abhishek Singh <abhisheksingh@Abhisheks-MacBook-Air.local>
2026-05-28 02:39:35 +0530

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,14 +6,22 @@ export default async function Header() {
   return (
     <header className="site-header">
       <div className="logo">
-        PORT ST. MARY’S
-        <br />
-        MARINA
+        <a href="/">
+          <img
+            src="https://grey-moose-264563.hostingersite.com/wp-content/uploads/2026/05/attachment7-1.png"
+            alt="Port St. Mary's Marina"
+            className="logo-img"
+          />
+        </a>
       </div>
 
       <nav>
         {menuItems.map((item: any) => (
-          <a key={item.ID || item.id} href={item.url} target={item.target || "_self"}>
+          <a
+            key={item.ID || item.id}
+            href={item.url}
+            target={item.target || "_self"}
+          >
             {item.title}
           </a>
         ))}
