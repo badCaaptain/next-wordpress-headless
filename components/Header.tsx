b104ab00c4d5df3f0b2dbb2efde42609938b3fc8
Abhishek Singh <abhisheksingh@Abhisheks-MacBook-Air.local>
2026-05-28 02:47:23 +0530

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,15 +5,35 @@ export default async function Header() {
 
   return (
     <header className="site-header">
-      <div className="logo">
-        <a href="/">
-          <img
-            src="https://grey-moose-264563.hostingersite.com/wp-content/uploads/2026/05/attachment7-1.png"
-            alt="Port St. Mary's Marina"
-            className="logo-img"
-          />
-        </a>
+      <div className="container">
+        <div className="flex-col justify-between gap-5 ">
+            <div className="logo">
+              <a href="/">
+                <img
+                  src="https://grey-moose-264563.hostingersite.com/wp-content/uploads/2026/05/attachment7-1.png"
+                  alt="Port St. Mary's Marina"
+                  className="logo-img"
+                />
+              </a>
+            </div>
+
+            <div className="flex-col gap-5">
+              <nav>
+                {menuItems.map((item: any) => (
+                  <a
+                    key={item.ID || item.id}
+                    href={item.url}
+                    target={item.target || "_self"}
+                  >
+                    {item.title}
+                  </a>
+                ))}
+              </nav>
+                <a href="#" className="header-btn">Reserve Now</a>
+            </div>
+        </div>
       </div>
+      
 
       <nav>
         {menuItems.map((item: any) => (
@@ -26,6 +46,8 @@ export default async function Header() {
           </a>
         ))}
       </nav>
+
+
     </header>
   );
 }
